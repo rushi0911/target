98be6ae28799b3477e7ba15db40e525c8a7db184
Rushikesh Manurkar <83496076+rushi0911@users.noreply.github.com>
2022-07-08 00:45:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Only created An layout 
+// The website is responsive that it will mange to change its lay out depeending upon which device we use
+// Sir i am not adding any javascript in-it because there is no button function given i.e what happen after click this button.
+// So i only created a layout using HTML & CSS only.
+// Sir i can do it using react also.
